ef8bac973fd0f85abffaa7ef985168e6b92400a8
Albina <kamalieva.albish@mail.ru>
2024-05-03 18:14:55 +0300

    Добавьте файлы проекта.

---
+++ Modul.11/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Xml.Xsl;
+
+namespace TelegramBot
+{
+    class Bot//класс инкапсулирует (заключает в себя) логику обработки входящих запросов
+    {
+        ///<summary>
+        ///объект, отвеающий за отправку сообщений клиенту
+        ///</summary>
+        private TelegramBotClient _telegramClient;
+
+        public Bot (TelegramBotClient telegramClient)
+        {
+            _telegramClient = telegramClient;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken cancellationToken)//метод регистрирующий обработки событий
+        {
+            _telegramClient.OnMessage += HandleMessage;
+            _telegramClient.OnMessage += HandleButtonClick;
+
+            Console.WriteLine("bot started");
+        }
+
+        ///<summary>
+        ///Обработчик входящих текстовых сообщений
+        /// </summary>
+        private async Task HandleMessage(object sender, MessageEventArgs e)
+        {
+            //бот получил входящее сообщение пользователя
+            var messageText = e.Message.Text;
+
+            //бот отправляет ответ
+            _telegramClient.SendTextMessage(e.ChatId, "Ответ на сообщение пользователя");
+        }
+
+        ///<summary>
+        ///Обработчик нажатий на кнопки
+        ///</summary>
+        private async Task HandleButtonClick(object sender, MessageEventArgs e)
+        {
+
+        }
+    }
+}
